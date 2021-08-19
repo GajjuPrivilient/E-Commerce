c0f80b6c75154cf8d9900dee1a6c381c678f2461
Gajju45 <gajendrasharma421@gmail.com>
2021-08-19 18:08:21 +0530

    Splash Screen OTP Mobile activity_otpactivity.xml Verification activity

---
+++ app/src/main/java/com/android/gajju45/e_commerce/Activities/OTPActivity.java
@@ -0,0 +1,16 @@
+package com.android.gajju45.e_commerce.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.gajju45.e_commerce.R;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpactivity);
+    }
+}
